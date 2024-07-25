6fd445248f3120093589505701d2aa23ad7860f5
moshcat <mauricecathoverile@gmail.com>
2024-07-25 14:45:21 +0800

    Fix navbar

--- components/ui/Navbar.vue
+++ components/ui/Navbar.vue
@@ -23,7 +23,7 @@
           </a>
           <button type="button" class="btn-close navbar__toggler" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
-        <div class="offcanvas-body">
+        <div class="offcanvas-body  ">
           <ul class="navbar-nav navbar__nav justify-content-end flex-grow-1 ">
             <li class="nav-item navbar__item  ">
               <a class="nav-link navbar__link navbar__link--active" aria-current="page" href="#">Home</a>
@@ -44,11 +44,12 @@
           </ul>
           <ul class=" navbar-nav navbar__nav language-switcher-desktop">
             <li class="dropdown navbar__item nav-item">
-              <a class=" dropdown-toggle bg-primary nav-link navbar__link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a class=" dropdown-toggle nav-link navbar__link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 <img src="~assets/images/icon/united-kingdom.png" alt="English">
               </a>
               <ul class="dropdown-menu">
-                <li class="navbar__item nav-item lang-item"><a class="dropdown-item" href="#">
+                <li class="navbar__item nav-item lang-item">
+                  <a class="dropdown-item p-1" href="#">
                   <img src="~assets/images/icon/indonesia.png" alt="Indonesia">
                 </a></li>
               </ul>
@@ -97,22 +98,20 @@
   .language-switcher-desktop{
     .dropdown-menu{
       min-width: unset;
-      box-shadow: gray;
+      width: 2.5em;
+      border-radius: 0px;
     }
-
-
 
     .lang-item {
       img {
-        width: 24px;
+        width: 28px;
         height: 24px;
       }
     }
 
     .navbar__link {
-      padding-right: 0;
-      border-bottom: none;
-
+      //padding-right: 0;
+      //border-bottom: none;
 
       img {
         width: 24px;
@@ -133,10 +132,6 @@
   }
   &__item {
     height: 100%;
-
-
-
-
   }
 
   &__link {
@@ -145,7 +140,7 @@
     position: relative;
     transition: width 1s;
     letter-spacing: 0.05em;
-    padding: 2.6041666667vw 1.0416666667vw;
+    padding: 2.2vw 1.0416666667vw;
     display: inline-flex;
 
 
@@ -158,7 +153,7 @@
       height: 1px;
       width: 100%;
       left: 0;
-      bottom: -8px;
+      bottom: -10px;
     }
     &:hover::after, &--active::after{
       opacity: 1;
@@ -170,6 +165,70 @@
       color: $primary;
     }
   }
+
+
+  @media (max-width: 991px){
+    padding: 24px 0px;
+
+    .language-switcher-desktop{
+      display: none;
+    }
+
+    .preschool-menu{
+      margin-right: 0;
+
+      .navbar__link::before {
+        display: none;
+      }
+      .navbar__link::after{
+        position: relative;
+        display: inline-block;
+        top: 5px;
+        left: 8px;
+        width: 10px;
+        height: 10px;
+      }
+    }
+
+    &__image-container {
+      max-width: 140px;
+    }
+
+    &__item {
+      width: 100%;
+
+      .dropdown-menu {
+        margin-top: 0;
+        padding: 0;
+        border-radius: 0;
+        border: none;
+      }
+
+      .dropdown-item {
+        padding: 18px 14px;
+        font-size: 16px;
+      }
+    }
+
+    &__link {
+      width: 100%;
+      padding: 18px 0px !important;
+      font-size: 16px;
+      line-height: initial;
+      border-bottom: 1px solid grey;
+
+      &--active {
+        border-bottom: 1px solid $primary;
+      }
+    }
+  }
+
+  @media(max-width: 575px){
+    &__offcanvas {
+      width: 100% !important;
+    }
+  }
+
 
 
 }
